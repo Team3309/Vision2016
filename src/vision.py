1a4d5fa3366b9ca887065e9b5abd8bfbbe7fad21
Vinnie Magro <vmagro@socaldevs.com>
2016-03-19 17:54:54 -0700

    Fix findContours based on OpenCV version

--- src/vision.py
+++ src/vision.py
@@ -171,7 +171,10 @@ def fix_target_perspective(contour, bin_shape):
         fixed_perspective = cv2.warpPerspective(before_warp, warp, shape)
         fixed_perspective = fixed_perspective.astype(np.uint8)
 
-        _, contours, _ = cv2.findContours(fixed_perspective, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if cv2.__version__ >= 3:
+            _, contours, _ = cv2.findContours(fixed_perspective, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        else:
+            contours, _ = cv2.findContours(fixed_perspective, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         new_contour = contours[0]
 
         return new_contour, fixed_perspective
@@ -292,7 +295,10 @@ def find(img, hue_min, hue_max, sat_min, sat_max, val_min, val_max, draw_output,
     if draw_output:
         output_images['bin'] = np.copy(bin)
 
-    _, contours, hierarchy = cv2.findContours(bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if cv2.__version__ >= 3:
+        _, contours, hierarchy = cv2.findContours(bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    else:
+        contours, hierarchy = cv2.findContours(bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # filter out so only left with good contours
     original_count = len(contours)
